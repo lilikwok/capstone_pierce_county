33397d083c99e99a744bf29850218e2f5ca23d09
lilikwok <guoqianhui@seattleu.edu>
2019-07-30 17:07:35 -0700

    update graph

--- decision_tree.py
+++ decision_tree.py
@@ -15,7 +15,6 @@ from sklearn.datasets import make_regression
 df = pd.read_csv("master_model.csv", sep=',', header=0)
 
 y = df['sale_price']
-print(df.dtypes)
 df['View_Quality'] = df['View_Quality'] .astype('category')
 df['Waterfront_Type'] = df['Waterfront_Type'] .astype('category')
 df['withInSewerImprovement'] = df['withInSewerImprovement'] .astype('category')
@@ -35,7 +34,7 @@ df['attached_garage_square_feet'] = df['attached_garage_square_feet'] .astype('c
 df['detached_garage_square_feet'] = df['detached_garage_square_feet'] .astype('category')
 df['fireplaces'] = df['fireplaces'] .astype('category')
 df['Crime_Num'] = df['Crime_Num'].fillna(0)
-print(df.columns)
+
 
 X = df[['Land_Net_Acres','View_Quality', 
        'Waterfront_Type', 'Crime_Num', 'withInSewerImprovement',
@@ -77,7 +76,7 @@ MSE = mean_squared_error(y, y_pred)
 print("MSE:",MSE)
 # export the decision tree to a tree.dot file 
 # for visualizing the plot easily anywhere 
-export_graphviz(regressor, out_file ='tree.dot',feature_names=X.columns)  
+export_graphviz(reg, out_file ='tree.dot',feature_names=X.columns)  
 
 #MSE: 4854304329.722403
 '''
@@ -117,12 +116,13 @@ export_graphviz(regressor, out_file ='tree1.dot',feature_names=X1.columns)
 ###############################################
 
 
-regr = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=6,
+regr = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=7,
            max_features='auto', max_leaf_nodes=None,
            min_impurity_decrease=0.0, min_impurity_split=None,
            min_samples_leaf=1, min_samples_split=2,
            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=None,
            oob_score=False, random_state=0, verbose=0, warm_start=False)
+
 
 rrtree = regr.fit(X, y)  
 y_pred = rrtree.predict(X)  
@@ -130,28 +130,9 @@ MSE = mean_squared_error(y, y_pred)
 print("MSE of random forest:",MSE)
 # export the decision tree to a tree.dot file 
 # for visualizing the plot easily anywhere 
-'''
-from sklearn.datasets import load_iris
-iris = load_iris()
 
-from sklearn.tree import export_graphviz
-
-export_graphviz(rrtree, out_file='random_forest.dot', 
-                feature_names = iris.feature_names,
-                class_names = iris.target_names,
-                rounded = True, proportion = False, 
-                precision = 2, filled = True)
-
-# Convert to png using system command (requires Graphviz)
-from subprocess import call
-call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
-
-# Display in jupyter notebook
-from IPython.display import Image
-Image(filename = 'tree.png')
-'''
-
-
-
+export_graphviz(regr.estimators_[0], out_file='tree_from_forest1.dot',feature_names=X.columns)
+export_graphviz(regr.estimators_[1], out_file='tree_from_forest2.dot',feature_names=X.columns)
+export_graphviz(regr.estimators_[2], out_file='tree_from_forest3.dot',feature_names=X.columns)
 # REFRENCE: https://www.geeksforgeeks.org/python-decision-tree-regression-using-sklearn/
 # MSE:https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
